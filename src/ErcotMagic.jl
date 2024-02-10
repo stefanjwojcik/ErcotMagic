ed34f3f37837b9810ad4987db6bc052114b19257
stefanjwojcik <stefan.wojcik@colorado.edu>
2024-02-10 16:05:38 -0700

    ercot api

--- src/ErcotMagic.jl
+++ src/ErcotMagic.jl
@@ -3,9 +3,27 @@ module ErcotMagic
 using HTTP
 using JSON
 
+mutable struct ErcotAPICall
+    base_url::String
+    params::Dict{String, String}
+end
+
+
+function ErcotAPICall(;base_url=nothing, params=nothing)
+    if base_url === nothing
+        error("base_url is required")
+    end
+    if params === nothing
+        params = Dict{String, String}()
+    end
+    new(base_url, params)
+end
+
+function 
+
 """
 # Example:
-data = fetch_ercot_data(delivery_date_from="2024-01-01", delivery_date_to="2024-01-31")
+data = fetch_ercot_dalmp_data(delivery_date_from="2024-01-01", delivery_date_to="2024-01-31")
 
 """
 function fetch_ercot_dalmp_data(;dst_flag=nothing, delivery_date_from=nothing, 
@@ -61,5 +79,17 @@ function fetch_ercot_dalmp_data(;dst_flag=nothing, delivery_date_from=nothing,
         error("Request failed with status code: ", response.status)
     end
 end
+
+
+"""
+# 60 Day DAM Energy Bid Awards
+# NP3-966-ER
+# Request for 60 day DAM Energy Bid Awards
+https://api.ercot.com/api/public-reports/np3-966-er/60_dam_energy_bid_awards[?deliveryDateFrom][&deliveryDateTo][&hourEndingFrom][&hourEndingTo][&settlementPointName][&qseName][&energyOnlyBidAwardInMWFrom][&energyOnlyBidAwardInMWTo][&settlementPointPriceFrom][&settlementPointPriceTo][&bidId][&page][&size][&sort][&dir]
+"""
+function fetch_ercot_60_day_dam_energy_bid_awards()
+end
+
+
 
 end # module ErcotMagic
