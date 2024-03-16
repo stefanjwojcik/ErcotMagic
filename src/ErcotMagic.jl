49b9cecf74cadd97f375428523febc66545b9534
= <=>
2024-03-16 14:37:43 -0500

    julia one works better

--- src/ErcotMagic.jl
+++ src/ErcotMagic.jl
@@ -2,94 +2,79 @@ module ErcotMagic
 
 using HTTP
 using JSON
+using DotEnv
 
-mutable struct ErcotAPICall
-    base_url::String
-    params::Dict{String, String}
-end
+DotEnv.config()
 
-
-function ErcotAPICall(;base_url=nothing, params=nothing)
-    if base_url === nothing
-        error("base_url is required")
-    end
-    if params === nothing
-        params = Dict{String, String}()
-    end
-    new(base_url, params)
-end
-
-function 
+const sixty_dam_energy_only_offers = "https://api.ercot.com/api/public-reports/np3-966-er/60_dam_energy_only_offers?"
+const sixty_dam_awards = "https://api.ercot.com/api/public-reports/np3-966-er/60_dam_energy_only_offer_awards?"
+const twodayAS = "https://api.ercot.com/api/public-reports/np3-911-er/2d_agg_as_offers_ecrsm?"
+const energybids = "https://api.ercot.com/api/public-reports/np3-966-er/60_dam_energy_bids?"
 
 """
-# Example:
-data = fetch_ercot_dalmp_data(delivery_date_from="2024-01-01", delivery_date_to="2024-01-31")
-
+# A function to retreive the auth token 
+token = get_auth_token()
 """
-function fetch_ercot_dalmp_data(;dst_flag=nothing, delivery_date_from=nothing, 
-                           delivery_date_to=nothing, hour_ending=nothing,
-                           bus_name=nothing, lmp_from=nothing, lmp_to=nothing,
-                           page=nothing, size=nothing, sort=nothing, dir=nothing)
-    
-    base_url = "https://api.ercot.com/api/public-reports/np4-183-cd/dam_hourly_lmp"
-    params = []
-    
-    if dst_flag !== nothing
-        push!(params, "DSTFlag=$dst_flag")
-    end
-    if delivery_date_from !== nothing
-        push!(params, "deliveryDateFrom=$delivery_date_from")
-    end
-    if delivery_date_to !== nothing
-        push!(params, "deliveryDateTo=$delivery_date_to")
-    end
-    if hour_ending !== nothing
-        push!(params, "hourEnding=$hour_ending")
-    end
-    if bus_name !== nothing
-        push!(params, "busName=$bus_name")
-    end
-    if lmp_from !== nothing
-        push!(params, "LMPFrom=$lmp_from")
-    end
-    if lmp_to !== nothing
-        push!(params, "LMPTo=$lmp_to")
-    end
-    if page !== nothing
-        push!(params, "page=$page")
-    end
-    if size !== nothing
-        push!(params, "size=$size")
-    end
-    if sort !== nothing
-        push!(params, "sort=$sort")
-    end
-    if dir !== nothing
-        push!(params, "dir=$dir")
-    end
-    
-    query_string = join(params, "&")
-    full_url = string(base_url, query_string |> isempty ? "" : "?", query_string)
-    
-    response = HTTP.get(full_url)
-    
-    if response.status == 200
-        return JSON.parse(String(response.body))
-    else
-        error("Request failed with status code: ", response.status)
-    end
+function get_auth_token()
+    API_URL = "https://ercotb2c.b2clogin.com/ercotb2c.onmicrosoft.com/B2C_1_PUBAPI-ROPC-FLOW/oauth2/v2.0/token?"
+    GRANT_TYPE ="password"
+    username=ENV["ERCOTUSER"]
+    password=ENV["ERCOTPASS"]
+    response_type="id_token"
+    scope="openid+fec253ea-0d06-4272-a5e6-b478baeecd70+offline_access"
+    client_id="fec253ea-0d06-4272-a5e6-b478baeecd70"
+
+    apicall = API_URL * "grant_type=" * GRANT_TYPE * "&username=" * username * "&password=" * password * "&response_type=" * response_type * "&scope=" * scope * "&client_id=" * client_id
+    response = HTTP.post(apicall, 
+        headers = ["Content-Type" => "application/x-www-form-urlencoded"]
+    )
+    return JSON.parse(String(response.body))
 end
 
-
 """
-# 60 Day DAM Energy Bid Awards
-# NP3-966-ER
-# Request for 60 day DAM Energy Bid Awards
-https://api.ercot.com/api/public-reports/np3-966-er/60_dam_energy_bid_awards[?deliveryDateFrom][&deliveryDateTo][&hourEndingFrom][&hourEndingTo][&settlementPointName][&qseName][&energyOnlyBidAwardInMWFrom][&energyOnlyBidAwardInMWTo][&settlementPointPriceFrom][&settlementPointPriceTo][&bidId][&page][&size][&sort][&dir]
+# Base call to ERCOT API 
+- authorization => token_bearer
+- headers => Ocp-Apim-Subscription-Key => ENV["ERCOTKEY"]
+- url => https://api.ercot.com/api/public-reports/np3-966-er/60_dam_energy_only_offers
+sixty_dam_energy_only_offers = "https://api.ercot.com/api/public-reports/np3-966-er/60_dam_energy_only_offers"
+response = ercot_api_call(token["id_token"], sixty_dam_energy_only_offers)
 """
-function fetch_ercot_60_day_dam_energy_bid_awards()
+function ercot_api_call(token_id, url)
+    response = HTTP.get(url, 
+        headers = ["Authorization" => "Bearer " * token_id
+            "Ocp-Apim-Subscription-Key" => ENV["ERCOTKEY"]
+            "Content-Type" => "application/json"
+        ]
+    )
+    return JSON.parse(String(response.body))
 end
 
+"""
+# Function to formulate a url for ERCOT API based on params in kwargs
+params = Dict("deliveryDateFrom" => "2021-08-01", "deliveryDateTo" => "2024-02-25")
+params2 = Dict("settlementPointName" => "HB_NORTH")
+url = ercot_api_url(params)
+# try to cal 
+response = ercot_api_call(token["id_token"], url)
 
+## Two day AS
+twodayasurl = ercot_api_url(params, twodayAS)
+response = ercot_api_call(token["id_token"], twodayasurl)
 
-end # module ErcotMagic
+## Sixty DAM awards
+sixty_dam_awards_url = ercot_api_url(params, sixty_dam_awards)
+response = ercot_api_call(token["id_token"], sixty_dam_awards_url)
+
+"""
+function ercot_api_url(params, url)
+    for (key, value) in params
+        url *= key * "=" * value * "&"
+    end
+    return url
+end 
+
+function parse_ercot_response(response)
+    # data is a vector of vectors, each of them is a row 
+    data = response["data"]
+
+end # module
