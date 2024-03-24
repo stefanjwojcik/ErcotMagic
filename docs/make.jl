7ac1de9b00a80827369ac9db49f25ca2def5d538
= <=>
2024-03-24 15:22:35 -0500

    works

--- docs/make.jl
+++ docs/make.jl
@@ -5,11 +5,5 @@ using Documenter, ErcotMagic
 #push!(LOAD_PATH,"../src/")
 #makedocs(sitename="ErcotMagic.jl", format="html")
 
-makedocs(
-    # options
-    sitename = "ErcotMagic.jl",
-    authors = "Stefan Wojcik",
-    modules = [ErcotMagic],
-    checkdocs = :exports,
-    format = "html"
-)
+makedocs(sitename = "ErcotMagic.jl", authors = "Stefan Wojcik", 
+modules = [ErcotMagic],checkdocs = :exports)
