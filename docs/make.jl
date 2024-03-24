5abf6b03d1bfb2d28eed45300902abda3ec7ac5b
= <=>
2024-03-24 15:22:28 -0500

    functioning mamke file

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,15 @@
 ## Make Documentation 
 
-using Documenter 
+using Documenter, ErcotMagic
 
-push!(LOAD_PATH,"../src/")
-makedocs(sitename="ErcotMagic.jl", format="html")
+#push!(LOAD_PATH,"../src/")
+#makedocs(sitename="ErcotMagic.jl", format="html")
+
+makedocs(
+    # options
+    sitename = "ErcotMagic.jl",
+    authors = "Stefan Wojcik",
+    modules = [ErcotMagic],
+    checkdocs = :exports,
+    format = "html"
+)
