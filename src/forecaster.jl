5a54240e5eaf1a2e0b73e1e8bda51ddf26d2e9de
= <=>
2024-04-01 20:50:57 -0500

    all experiments work w/ plots

--- src/forecaster.jl
+++ src/forecaster.jl
@@ -1,6 +1,8 @@
 ## Forecaster - uses recent values to predict future values
 
-using Flux, DotEnv, DataFrames, ProgressMeter, CUDA
+using Flux, DotEnv, DataFrames, ProgressMeter, CUDA, ErcotMagic, Statistics
+# disallow scalar GPU 
+CUDA.allowscalar(false)
 
 DotEnv.config()
 
@@ -16,27 +18,42 @@ end
 # Create batches of a time series, X is the time series, s is the sequence length, r is the step size
 # If you start with a univariate time series of size 1 x T, you will get s x (T - s) / r batches
 """
-function process_data(X, seq_len=60)
+function process_data(Xraw, seq_len=60)
     # X_tr is a list of sequences of length seq_len
-    X_tr = [(X[i:i+seq_len-1]) for i in 1:length(X)-seq_len]
+    X = [(Xraw[i:i+seq_len-1]) for i in 1:length(Xraw)-seq_len]
     # Y is the next value in the time series
-    Y_tr = [X[i+seq_len] for i in 1:length(X)-seq_len]
-    return X_tr, Y_tr
+    Y = [Xraw[i+seq_len] for i in 1:length(Xraw)-seq_len]
+    return X, Y
 end
 
 ##################################################
 
+"""
 # Function to train the forecaster
-function train_forecaster(model, X_tr, Y_tr, epochs)
+# model: The model to train
+m = Chain(
+    Dense(seq_len, seq_len, tanh), # Notice the k inputs
+    Dense(seq_len, seq_len, tanh),
+    Dense(seq_len, 1, identity)
+) |> gpu
+X_tr, Y_tr = process_data(dat_train, 60)
+train_forecaster(m, cu.(X_tr), cu.(Y_tr), 10, 0.0)
+"""
+function train_forecaster(model, X_tr, Y_tr, epochs, penalty_weight=0.0)
     opt = ADAM()
-    θ = Flux.params(m) # Keep track of the parameters
-    for i in 1:epochs
+    penalty() = [sum(abs2, m.weight) + sum(abs2, m.bias) for m in model.layers[1:end-1]] |> sum
+    θ = Flux.params(model) # Keep track of the parameters
+    @showprogress for i in 1:epochs
      # Compute gradients
         ∇ = gradient(θ) do 
             # Warm up model
             model(X_tr[1])
             # Compute MSE loss on rest of sequence
-            Flux.Losses.mse.([model(x) for x ∈ X_tr[2:end]], Y_tr[2:end]) |> mean
+            if penalty == 0.0
+                Flux.Losses.mse.([model(x) for x ∈ X_tr[2:end]], Y_tr[2:end]) |> mean
+            else
+                (Flux.Losses.mse.([model(x) for x ∈ X_tr[2:end]], Y_tr[2:end]) |> mean) + penalty() * penalty_weight
+            end
         end
     Flux.update!(opt, θ, ∇)
     end
@@ -52,9 +69,6 @@ rt_dat = get_ercot_data(params, ErcotMagic.rt_prices)
 ## Convert to Float32
 dat_train = Float32.(rt_dat.LMP)
 
-# Process the data
-#X_train, Y_train = process_data(X_tr, seq_len)
-
 ###### VALIDATION DATA ######
 ## Download the next day's data for validation 
 params = Dict("RTDTimestampFrom" => "2024-02-02T00:00:00", 
@@ -62,6 +76,9 @@ params = Dict("RTDTimestampFrom" => "2024-02-02T00:00:00",
                 "settlementPoint" => "HB_NORTH")
 rt_datval = get_ercot_data(params, ErcotMagic.rt_prices)
 dat_valid = Float32.(rt_datval.LMP)
+
+#### ALL DATA ####
+alldata = dat_train, dat_valid
 
 ### Validation data 
 #X_val, Y_val = process_data(dat_valid, seq_len)
@@ -73,10 +90,34 @@ function plot_ts(m, X_tr, Y_tr)
     plot([ŷ, Y_tr], label=["Predictions" "Actuals"])
 end
 
-function finalEval(m, X_valid)
-    X_val = cu.(X_valid)
-    preds = [m(x)[1] for x ∈ X_val]
-    mse = Flux.Losses.mse(preds, Y_val)
+"""
+## Recipe for plotting ExperimentResults 
+plot(expResults1, Y_val)
+"""
+@recipe function f(er::ExperimentResults, actuals)
+    # Get the predictions
+    preds = first.(cpu(er.predictions))
+    ## Plot the predictions
+    time = 1:length(preds)
+    label --> ["Predictions" "Actuals"]
+    return time, [preds, actuals]
+end
+
+"""
+# Function to evaluate the model
+# m: The model
+m = Chain(
+    Dense(seq_len, seq_len, tanh), # Notice the k inputs
+    Dense(seq_len, 1, identity)
+) |> gpu
+train_forecaster(m, cu.(X_tr), cu.(Y_tr), 10)
+X_val, Y_val = process_data(dat_valid, 60)
+finalEval(m, X_val, Y_val)
+"""
+function finalEval(m, X_val, Y_val)
+    X_val, Y_val = cu.(X_val), cu.(Y_val)
+    preds = m.(X_val)
+    mse = Flux.Losses.mse(first.(cpu(preds)), Y_val)
     return ExperimentResults(m, preds, mse)
 end
 
@@ -87,6 +128,7 @@ Experiment 1: Simple Model
 alldata = dat_train, dat_valid
 seq_len = 60
 expResults1 = experiment1(alldata, 60, 100)
+p1 = plot(expResults1, Y_val)
 """
 function experiment1(alldata, seq_len=60, n_epochs=10000)
     # Unpack alldata
@@ -103,19 +145,20 @@ function experiment1(alldata, seq_len=60, n_epochs=10000)
     train_forecaster(m, cu.(X_tr), cu.(Y_tr), n_epochs)
     # predict on validation data
     X_valid, Y_valid = process_data(X_valid, seq_len)
-    finalEval(m, X_valid)
+    finalEval(m, X_valid, Y_valid)
 end
 
 """
 Experiment 2: Adding Mean and Standard Deviation
 alldata = dat_train, dat_valid
 expResults2 = experiment2(alldata, 60, 100)
+p2 = plot(expResults2, Y_val)
 """
-function experiment2(alldata, seq_len=60, n_epochs=10000)
+function experiment2(alldata, seq_len=60, n_epochs=100)
     # Unpack alldata
-    X_tr, X_val = alldata
+    X_trainraw, X_valraw = alldata
     # Process the data
-    X_tr, Y_tr = process_data(X_tr, seq_len)
+    X_tr, Y_tr = process_data(X_trainraw, seq_len)
     # add the moving mean and moving standard deviation by batch
     for i in 1:length(X_tr)
         push!(X_tr[i], mean(X_tr[i]))
@@ -128,7 +171,7 @@ function experiment2(alldata, seq_len=60, n_epochs=10000)
         Dense(seq_len + 2, 1, identity)
     ) |> gpu
     # Generate validation data
-    X_val, Y_val = process_data(X_val, seq_len)
+    X_val, Y_val = process_data(X_valraw, seq_len)
     # add the moving mean and moving standard deviation by batch
     for i in 1:length(X_val)
         push!(X_val[i], mean(X_val[i]))
@@ -136,13 +179,14 @@ function experiment2(alldata, seq_len=60, n_epochs=10000)
     end
     # Train the model
     train_forecaster(m, cu.(X_tr), cu.(Y_tr), n_epochs)
-    finalEval(m, X_val)
+    finalEval(m, X_val, Y_val)
 end
 
 """
 Experiment 3: Going Deeper Layers
 alldata = dat_train, dat_valid
 expResults3 = experiment3(alldata, 60, 100)
+p3 = plot(expResults3, Y_val)
 """
 function experiment3(alldata, seq_len=60, n_epochs=10000)
     # Unpack alldata
@@ -172,13 +216,16 @@ function experiment3(alldata, seq_len=60, n_epochs=10000)
     # Train the model
     train_forecaster(m, cu.(X_tr), cu.(Y_tr), n_epochs)
     # predict on validation data
-    finalEval(m, X_val)
+    finalEval(m, X_val, Y_val)
 end
 
 """
 Experiment 4: Adding Regularization
+alldata = dat_train, dat_valid
+expResults4 = experiment4(alldata, 60, 100, .1)
+p4 = plot(expResults4, Y_val)
 """
-function experiment4(alldata, seq_len=60, n_epochs=10000)
+function experiment4(alldata, seq_len=60, n_epochs=10000, penalty=0.1)
     # Unpack alldata
     X_tr, X_val = alldata
     # Process the data
@@ -190,21 +237,21 @@ function experiment4(alldata, seq_len=60, n_epochs=10000)
     end    
     # Create a model
     m = Chain(
-        Dense(seq_len + 4, seq_len + 4, tanh), # Notice the k inputs
-        Dense(seq_len + 4, seq_len + 4, tanh),
-        Dense(seq_len + 4, seq_len + 4, tanh),
-        Dense(seq_len + 4, seq_len + 4, tanh),
-        Dense(seq_len + 4, 1, identity)
-    ) 
+        Dense(seq_len + 2, seq_len + 2, tanh), # Notice the k inputs
+        Dense(seq_len + 2, seq_len + 2, tanh),
+        Dense(seq_len + 2, seq_len + 2, tanh),
+        Dense(seq_len + 2, seq_len + 2, tanh),
+        Dense(seq_len + 2, 1, identity)
+    ) |> gpu
     # Generate validation data
-    X_val, Y_val = process_data(X_val, seq_len)
+    X_val, Y_valid = process_data(X_val, seq_len)
     # add the moving mean and moving standard deviation by batch
     for i in 1:length(X_val)
         push!(X_val[i], mean(X_val[i]))
         push!(X_val[i], std(X_val[i]))
     end
     # Train the model
-    train_forecaster(m, cu.(X_tr), cu.(Y_tr), n_epochs)
+    train_forecaster(m, cu.(X_tr), cu.(Y_tr), n_epochs, penalty)
     # predict on validation data
-    finalEval(m, X_valid, seq_len)
+    finalEval(m, X_val, Y_valid)
 end
