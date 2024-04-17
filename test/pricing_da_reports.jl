205619810738079d8a767daf6cf8fc6f89d9e02e
stefanjwojcik <stefan.wojcik@colorado.edu>
2024-04-17 15:52:05 -0500

    filterload

--- test/pricing_da_reports.jl
+++ test/pricing_da_reports.jl
@@ -1,15 +1,17 @@
 ## Functions to parse 60 day energy offer data! 
 
 ## Generator data - contains awarded amounts, curves, and resource type
-params = Dict("deliveryDateFrom" => "2024-02-02", 
-                "deliveryDateTo" => "2024-02-03", 
-                "size" => "100")
-gen_dat = get_ercot_data(params, gen_data)
-## Remove spaces from the column names
-gen_dat = rename(gen_dat, replace.(names(gen_dat), " " => "_"))
-## Tabulate 
+allfiles = readdir(joinpath("data"))
+# Filter to those with DA_energy in name 
+da_energy_files = filter(x -> occursin("DA_energy", x), allfiles)
 
 # - need to get a dense enough sample of data from each plant to get an average sense of their curve
 # - how many segments does each plant typically offer? 
 # - what is the median $/mwh segment price value per plant? 
 # - what is the distribution of the median segment price? by fuel type?
+
+### Function to open a single .jld file and parse 
+function parse_da_energy_file(file)
+    data = @load joinpath("data", file)
+    return data
+end
