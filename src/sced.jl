25a28ab9f31afa1eb75f163ccb5508f9fd2d7c73
= <=>
2024-10-12 08:30:09 -0500

    sced

--- src/sced.jl
+++ src/sced.jl
@@ -69,3 +69,63 @@ function average_sced_mws(dat)
     dat = combine(groupby(dat, [:Resource_Name, :Resource_Type, :HSL]), :total_TPO_MW => mean)
     return dat
 end
+
+function DA_energy_offers(; kwargs...)
+    # From/To Dates are individual days
+    from = get(kwargs, :from, Date(today()) - Dates.Day(89) )
+    to = get(kwargs, :to, from + Dates.Day(1))
+    onpeak = get(kwargs, :onpeak, true)
+    if onpeak
+        hefrom = 7
+        heto = 22
+    else
+        hefrom = 0
+        heto = 24
+    end
+    params = Dict("deliveryDateFrom" => string(from), 
+                "deliveryDateTo" => string(to), 
+                "hourEndingFrom" => string(hefrom),
+                "hourEndingTo" => string(heto),
+                "size" => "1000000", 
+                "energyOnlyOfferMW1From" => "5", 
+                "energyOnlyOfferMW1To" => "10000",
+                "energyOnlyOfferPrice1From" => "-40",
+                "energyOnlyOfferPrice1To" => "4000")
+    get_ercot_data(params, ErcotMagic.sixty_dam_energy_only_offers)
+end
+
+"""
+# Function to update the DA offer data
+## Focuses on on-peak offers (he 7-22)
+"""
+function update_da_offer_data()
+    ## starting Date(today()) - Dates.Day(89)
+    startdate = Date(today()) - Dates.Day(89)
+    enddate = Date(today()) - Dates.Day(60)
+    @showprogress for offerday in startdate:enddate
+        try
+            # check if already exists 
+            isfile("data/DA_energy_offers_"*string(offerday)*".csv") && continue
+            dat = DA_energy_offers(from=offerday, to=offerday+Dates.Day(1), onpeak=true)
+            #transform nothing to missing 
+            dat = ErcotMagic.nothing_to_missing.(dat)
+            CSV.write("data/DA_energy_offers_"*string(offerday)*".csv", dat)
+        catch e
+            println("Error on date: ", offerday)
+            println(e)
+        end
+    end
+end
+
+function average_da_mws(dat)
+    dat = rename(dat, replace.(names(dat), " " => "_"))
+    ## Remove dashes from the column names
+    dat = rename(dat, replace.(names(dat), "-" => "_"))
+    # Add all TPO MW's together
+    datmw = select(dat, r"Energy_Only_Offer_MW")
+    datmw = coalesce.(datmw, 0.0)
+    # get rowwise max of tpo cols 
+    dat.avg_max_DA_mw_offer = maximum.(eachrow(datmw))
+    ## group by Resource_Name, Resource_Type,  
+    dat = combine(groupby(dat, [:Settlement_Point, :QSE]), :avg_max_DA_mw_offer => mean)
+end
