b1cac5eff69704890898626a5de66e920355b4c0
= <=>
2024-10-20 09:32:34 -0500

    mod to try catch

--- src/aws.jl
+++ src/aws.jl
@@ -32,13 +32,18 @@ download_csv_from_s3("ercotmagic", "data.csv", "data.csv")
 """
 function download_csv_from_s3(bucket::String, s3_key::String, download_path::String)
     # Download the file from S3
-    content = AWSS3.s3_get(bucket, s3_key)
+    try
+        content = AWSS3.s3_get(bucket, s3_key)
+    catch e
+        return DataFrame()
+    end
     
     # Save the file locally
     file = open(download_path, "w")
     write(file, content)
     close(file)
     println("File downloaded successfully to $download_path")
+    return CSV.read(download_path, DataFrame)
 end
 
 # Example usage
