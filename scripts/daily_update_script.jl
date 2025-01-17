dc4b4e6bad9e119e38a86961201662de761f83b1
= <=>
2025-01-17 16:02:43 -0600

    daily update works for non-sced

--- scripts/daily_update_script.jl
+++ scripts/daily_update_script.jl
@@ -2,8 +2,9 @@
 using ErcotMagic 
 ## This script is used to update the data from the API. It is run daily to get the latest data. The data is then saved in a csv file and pushed to an S3 bucket.
 
-function daily_nonsced_update()
-    startdate = today() - Day(7)
+function daily_nonsced_update(;kwargs...)
+    startdate = get(kwargs, :startdate, today() - Day(1))
+    enddate = get(kwargs, :enddate, today() - Day(1))
     enddate = today() - Day(1)
     addparams = Dict("size" => "1000000")
     #last_updated = ErcotMagic.get_last_updated()
@@ -12,8 +13,17 @@ function daily_nonsced_update()
     datout = DataFrame[]
     for endpoint in non_sced_endpoints
         println("Processing endpoint: ", endpoint)
-        dat = ErcotMagic.process_one_endpoint(startdate, enddate, endpoint, additional_params=addparams)
-        push!(datout, dat)
+        try
+            dat = ErcotMagic.process_one_endpoint(startdate, enddate, endpoint, additional_params=addparams)
+            push!(datout, dat)
+        catch e
+            println("Error processing endpoint: ", endpoint)
+            println(e)
+        end
     end
     return vcat(datout...)
 end 
+
+## Now Function to Update the SCED Data 
+function daily_sced_update()
+end
