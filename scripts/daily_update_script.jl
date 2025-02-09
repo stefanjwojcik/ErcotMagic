e432adf52909a3482f77ae627e39fde5d76e47d7
= <=>
2025-02-09 12:43:46 -0600

    dynamic updating

--- scripts/daily_update_script.jl
+++ scripts/daily_update_script.jl
@@ -2,26 +2,32 @@
 using ErcotMagic, Dates, DataFrames 
 ## This script is used to update the data from the API. It is run daily to get the latest data. The data is then saved in a csv file and pushed to an S3 bucket.
 
+function get_start_date(endpoint::String)
+    bq_start_date = ErcotMagic.bq("SELECT MAX(DATETIME(DATETIME)) FROM ercot." * endpoint)
+    bq_start_date = isnothing(bq_start_date) ? Date(2023, 12, 13) : Date(bq_start_date[1, 1])
+    return bq_start_date
+end
+
 function daily_nonsced_update(;kwargs...)
-    startdate = get(kwargs, :startdate, today() - Day(1))
-    enddate = get(kwargs, :enddate, today() - Day(1))
-    enddate = today() - Day(1)
+    #enddate = get(kwargs, :enddate, today() - Day(1))
+    #enddate = today() - Day(1)
     addparams = Dict("size" => "1000000")
     #last_updated = ErcotMagic.get_last_updated()
     non_sced_endpoints = ErcotMagic.get_non_sced_endpoints()
-    ## Non-SCED Endpoints 
-    datout = DataFrame[]
     for endpoint in non_sced_endpoints
         println("Processing endpoint: ", endpoint)
         try
-            dat = ErcotMagic.process_one_endpoint(startdate, enddate, endpoint, additional_params=addparams)
-            push!(datout, dat)
+            # Queries the BigQuery table to get the last updated date
+            bq_start_date = get_start_date(endpoint)
+            dat = ErcotMagic.process_one_endpoint(bq_start_date, today(), endpoint, additional_params=addparams)
+            # Stores the data at endpoint specific location
+            ErcotMagic.send_to_bq_table(dat, "ercot", endpoint)
         catch e
             println("Error processing endpoint: ", endpoint)
             println(e)
         end
     end
-    return vcat(datout...)
+    println("Non-SCED data updated successfully")
 end 
 
 ## Now Function to Update the SCED Data 
