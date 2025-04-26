9e9bc6b56da5ebc493c4b23136c645ef6915fce8
= <=>
2025-04-25 20:53:17 -0500

    parse endpoints

--- src/dev/openapi.jl
+++ src/dev/openapi.jl
@@ -9,6 +9,16 @@ open_spec = download(open_spec_url) |> JSON.parsefile
 # Get a valid server path 
 ercot_api_url = open_spec["servers"][1]["url"]
 
+# Gives the open_spec
+function parse_all_endpoints()
+    # OpenAPI spec for the ERCOT API
+    open_spec_url = "https://raw.githubusercontent.com/ercot/api-specs/refs/heads/main/pubapi/pubapi-apim-api.json"
+    # Load the Open API spec
+    open_spec = download(open_spec_url) |> JSON.parsefile
+    allurls = [open_spec["servers"][1]["url"]*x for x in keys(open_spec["paths"])]
+end
+
+
 mutable struct ErcotSpec
     openapi::String
     servers::Vector{Url}
